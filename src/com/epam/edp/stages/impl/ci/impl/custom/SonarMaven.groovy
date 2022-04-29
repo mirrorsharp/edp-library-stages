7a77f4d54e3f20f410e91dc2c744e6c6ebd4233b
Mykyta Leshchenko <mykyta_leshchenko@epam.com>
2022-04-29 11:36:54 +0300

    custom lib

--- src/com/epam/edp/stages/impl/ci/impl/custom/SonarMaven.groovy
+++ src/com/epam/edp/stages/impl/ci/impl/custom/SonarMaven.groovy
@@ -21,11 +21,3 @@ import com.epam.edp.stages.impl.ci.impl.sonarcleanup.SonarCleanup
 import com.epam.edp.tools.SonarScanner
 import org.apache.commons.lang.RandomStringUtils
 
-@Stage(name = "sonar", buildTool = ["maven"], type = [ProjectType.APPLICATION, ProjectType.AUTOTESTS, ProjectType.LIBRARY])
-class CustomSonar {
-    Script script
-    void run(context) {
-        script.sh "echo 'Your custom logic of the stage'"
-    }
-}
-return CustomSonar
--- src/com/epam/edp/stages/impl/ci/impl/sonar/SonarMaven.groovy
+++ src/com/epam/edp/stages/impl/ci/impl/sonar/SonarMaven.groovy
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.*/
 
-package com.epam.edp.stages.impl.ci.impl.custom
+package com.epam.edp.stages.impl.ci.impl.custom.SonarMaven
 
 
 import com.epam.edp.stages.impl.ci.ProjectType
@@ -21,79 +21,11 @@ import com.epam.edp.stages.impl.ci.impl.sonarcleanup.SonarCleanup
 import com.epam.edp.tools.SonarScanner
 import org.apache.commons.lang.RandomStringUtils
 
-@Stage(name = "sonar", buildTool = "maven", type = [ProjectType.APPLICATION, ProjectType.AUTOTESTS, ProjectType.LIBRARY])
-class SonarMaven {
+@Stage(name = "sonar", buildTool = ["maven"], type = [ProjectType.APPLICATION, ProjectType.AUTOTESTS, ProjectType.LIBRARY])
+class CustomSonar {
     Script script
-
     void run(context) {
-
         script.sh "echo 'Your custom logic of the stage'"
-
-
-        def codereviewAnalysisRunDir = context.workDir
-        if (context.job.type == "codereview") {
-            codereviewAnalysisRunDir = new File("${context.workDir}/../${RandomStringUtils.random(10, true, true)}")
-
-            script.dir("${codereviewAnalysisRunDir}") {
-                if (script.fileExists("${context.workDir}/target")) {
-                    script.println("[JENKINS][DEBUG] Project with usual structure")
-                    script.sh """
-                      export LANG=en_US.utf-8
-                      cd ${context.workDir}
-                      git config --local core.quotepath false
-                      IFS=\$'\\n';for i in \$(git diff --diff-filter=ACMR --name-only origin/${context.git.branch}); \
-                        do cp --parents \"\$i\" ${codereviewAnalysisRunDir}/; echo "file for scanner:" \"\$i\"/; done
-                      cp -f pom.xml ${codereviewAnalysisRunDir}/
-                      [ -d "src/test/" ] &&  cp --parents -r src/test/ ${codereviewAnalysisRunDir} || echo "Directory src/test/ not found."
-                      cp --parents -r target/ ${codereviewAnalysisRunDir}
-                      """
-                } else {
-                    script.println("[JENKINS][DEBUG] Multi-module project")
-                    script.sh """
-                      mkdir -p ${codereviewAnalysisRunDir}/unittests
-                      cd ${context.workDir}
-                      IFS=\$'\\n';for i in \$(git diff --diff-filter=ACMR --name-only origin/${context.git.branch}); \
-                        do cp --parents \"\$i\" ${codereviewAnalysisRunDir}/; echo "file for scanner:" \"\$i\"/; done
-                      for directory in `find . -type d -name \'test\'`; do cp --parents -r \${directory} \
-                      ${codereviewAnalysisRunDir}/unittests; done
-                      for poms in `find . -type f -name \'pom.xml\'`; do cp --parents -r \${poms} \
-                      ${codereviewAnalysisRunDir}; done
-                      for targets in `find . -type d -name \'target\'`; do cp --parents -r \${targets} \
-                      ${codereviewAnalysisRunDir}; done
-                      """
-                }
-            }
-        }
-        SonarScanner sonarScanner = new SonarScanner(script);
-        def buildTool = context.buildTool;
-        def path = "target/sonar";
-        def credentialsId = context.nexus.credentialsId;
-        def codebaseName;
-        def workDir;
-        if (context.job.type == "codereview" && context.codebase.config.strategy != "import") {
-            codebaseName = "${context.codebase.name}-${context.git.normalizedBranch}:change-${context.git.changeNumber}-${context.git.patchsetNumber}";
-            workDir = codereviewAnalysisRunDir;
-        } else {
-            codebaseName = "${context.codebase.name}-${context.git.normalizedBranch}";
-            workDir = context.workDir;
-        }
-        def scriptText = """ ${buildTool.command} ${buildTool.properties} -Dartifactory.username=LOGIN_REPLACE -Dartifactory.password=PASSWORD_REPLACE \
-                             sonar:sonar -Dsonar.projectKey=${codebaseName} \
-                             -Dsonar.projectName=${codebaseName} """;
-        if (context.job.type == "build") {
-            new SonarCleanup(script: script).run(context)
-        }
-        if (context.job.type == "codereview" && context.codebase.config.strategy != "import") {
-            sonarScanner.sendSonarScanWithCredentials(workDir, credentialsId, scriptText)
-
-            def report = script.readProperties file: "${workDir}/${path}/report-task.txt"
-            def ceTaskUrl = report.ceTaskUrl
-            sonarScanner.waitForSonarAnalysis(ceTaskUrl)
-
-            sonarScanner.waitForQualityGate()
-            return
-        }
-        sonarScanner.sendSonarScanWithCredentials(workDir, credentialsId, scriptText)
-        sonarScanner.waitForQualityGate()
     }
 }
+return CustomSonar

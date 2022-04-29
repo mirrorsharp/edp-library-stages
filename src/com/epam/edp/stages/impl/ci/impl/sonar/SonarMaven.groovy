62dca75e766f89aab05b186c8232d96ddde5c552
Mykyta Leshchenko <mykyta_leshchenko@epam.com>
2022-04-29 11:01:47 +0300

    custom lib

--- src/com/epam/edp/stages/impl/ci/impl/sonar/SonarMaven.groovy
+++ src/com/epam/edp/stages/impl/ci/impl/sonar/SonarMaven.groovy
@@ -12,7 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.*/
 
-// package com.epam.edp.stages.impl.ci.impl.sonar
+package com.epam.edp.stages.impl.ci.impl.sonar
 
 
 // import com.epam.edp.stages.impl.ci.ProjectType
